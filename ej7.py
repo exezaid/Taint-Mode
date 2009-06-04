6bf3658c2b41534a9d6bac886fd573e3d0746a52
jjconti <jjconti@07477463-646b-0410-87cb-c49d6d69b0f2>
2009-06-04 22:17:17 +0000

    Ej con regex.
    git-svn-id: http://svn.juanjoconti.com.ar/dyntaint@8 07477463-646b-0410-87cb-c49d6d69b0f2

---
+++ ej7.py
@@ -0,0 +1,35 @@
+from dyntaint import *
+
+# Mark unstrusted sources
+
+@untrusted
+def obtener_numero(mensaje="Ingrese un numero: "):
+    n = raw_input(mensaje)
+    return n
+
+@cleaner(SQLI)
+def limpiarSQLi(s):
+    '''lo limpie, creeme.'''
+    return s
+
+@ssinc(SQLI)
+def guardarDB(valor):
+    print "Guardando en la BD:", valor
+
+@ssinc(XSS)
+def mostrarPagina(valor):
+    print '<html>%s</html>' % (valor,)
+
+@ssinc()
+def sensible(valor):
+    print valor
+
+if __name__ == '__main__':
+    import re
+    n = obtener_numero()
+    # n esta manchada
+    guardarDB(n)
+    a, b, c = re.findall( '(.*?),(.*?),(.*?)', n )[0]
+    sensible(a)
+    sensible(b)
+    sensible(c)
