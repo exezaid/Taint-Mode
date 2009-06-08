24bf8cb97ab287211d63fb37f6f5a6b2da95a510
jjconti <jjconti@07477463-646b-0410-87cb-c49d6d69b0f2>
2009-06-08 22:57:18 +0000

    Correcciones de forma en test.
    git-svn-id: http://svn.juanjoconti.com.ar/dyntaint@13 07477463-646b-0410-87cb-c49d6d69b0f2

--- test.py
+++ test.py
@@ -7,177 +7,94 @@ def reached(v=None):
 
 @untrusted
 def some_input():
+    '''Some random input from the 'outside'.'''
     return "#%&#$%<--&" * random.randint(1, 10)
 
 @cleaner(SQLI)
-def limpiarSQLi(s):
+def cleanSQLI(s):
+    '''Dummy SQL injection cleaner.'''
     return s.replace("--", "")
 
 @cleaner(XSS)
-def limpiarSQLi(s):
+def cleanXSS(s):
+    '''Dummy XSS cleaner.'''
     return s.replace("<", "&lt;")
 
 @ssink(reached=reached)
-def guardarDB(valor):
+def saveDB(valor):
+    '''Dummy save in database function.'''
     return True
     
 class TestDifferenteVulnerabilities(unittest.TestCase):
 
-    def setup(self):
-        pass
-        
     def test_tainted(self):
         '''a tainted value reaches a sensitive sink.'''
-        
-        def reached(v=None):
-            return False
-        
-        @untrusted
-        def some_input():
-            return "#%&#$%<--&" * random.randint(1, 10)
-
-        @ssink(reached=reached)
-        def guardarDB(valor):
-            return True
             
         i = some_input()
-        self.assertFalse(guardarDB(i))
+        self.assertFalse(saveDB(i))
 
     def test_tainted_not_clean_anough(self):
         '''a partial tainted value reaches a full sensitive sink.'''
-        
-        def reached(v=None):
-            return False
-        
-        @untrusted
-        def some_input():
-            return "#%&#$%<--&" * random.randint(1, 10)
 
-        @cleaner(SQLI)
-        def cleanSQLI(s):
-            return s.replace("--", "")
-    
-        @ssink(reached=reached)
-        def guardarDB(valor):
-            return True
-            
         i = some_input()
-        self.assertFalse(guardarDB(cleanSQLI(i)))
+        self.assertFalse(saveDB(cleanSQLI(i)))
 
     def test_not_tainted(self):
         '''an SQLI-cleaned value reaches a SQLI-sensitive sink. It's all right.'''
-        
-        def reached(v=None):
-            return False
-        
-        @untrusted
-        def some_input():
-            return "#%&#$%<--&" * random.randint(1, 10)
 
-        @cleaner(SQLI)
-        def cleanSQLI(s):
-            return s.replace("--", "")
-    
         @ssink(v=SQLI, reached=reached)
-        def guardarDB(valor):
+        def saveDB(valor):
             return True
             
         i = some_input()
-        self.assertTrue(guardarDB(cleanSQLI(i)))
+        self.assertTrue(saveDB(cleanSQLI(i)))
 
 
 class TestTaintFlow(unittest.TestCase):
 
-    def setup(self):
-        pass
-
     def test_right_concatenation_not_cleaned(self):
         '''a tainted value is right concatenated with a non tainted value.
         The result is tainted. If not cleaned, the taint reaches the sink.'''
-        
-        def reached(v=None):
-            return False
-        
-        @untrusted
-        def some_input():
-            return "#%&#$%<--&" * random.randint(1, 10)
 
-        @cleaner(SQLI)
-        def cleanSQLI(s):
-            return s.replace("--", "")
-    
         @ssink(v=SQLI, reached=reached)
-        def guardarDB(valor):
+        def saveDB(valor):
             return True
             
         i = some_input()
-        self.assertFalse(guardarDB(i + "hohoho"))
+        self.assertFalse(saveDB(i + "hohoho"))
 
     def test_left_concatenation_not_cleaned(self):
         '''a tainted value is left concatenated with a non tainted value.
         The result is tainted. If not cleaned, the taint reaches the sink.'''
-        
-        def reached(v=None):
-            return False
-        
-        @untrusted
-        def some_input():
-            return "#%&#$%<--&" * random.randint(1, 10)
 
-        @cleaner(SQLI)
-        def cleanSQLI(s):
-            return s.replace("--", "")
-    
         @ssink(v=SQLI, reached=reached)
-        def guardarDB(valor):
+        def saveDB(valor):
             return True
             
         i = some_input()
-        self.assertFalse(guardarDB("hohoho" + i))
+        self.assertFalse(saveDB("hohoho" + i))
                     
     def test_right_concatenation(self):
         '''a tainted value is right concatenated with a non tainted value.
         The result is tainted.'''
-        
-        def reached(v=None):
-            return False
-        
-        @untrusted
-        def some_input():
-            return "#%&#$%<--&" * random.randint(1, 10)
 
-        @cleaner(SQLI)
-        def cleanSQLI(s):
-            return s.replace("--", "")
-    
         @ssink(v=SQLI, reached=reached)
-        def guardarDB(valor):
+        def saveDB(valor):
             return True
             
         i = some_input()
-        self.assertTrue(guardarDB(cleanSQLI(i + "hohoho")))
+        self.assertTrue(saveDB(cleanSQLI(i + "hohoho")))
 
     def test_left_concatenation(self):
         '''a tainted value is left concatenated with a non tainted value.
         The result is tainted.'''
-        
-        def reached(v=None):
-            return False
-        
-        @untrusted
-        def some_input():
-            return "#%&#$%<--&" * random.randint(1, 10)
-
-        @cleaner(SQLI)
-        def cleanSQLI(s):
-            return s.replace("--", "")
     
         @ssink(v=SQLI, reached=reached)
-        def guardarDB(valor):
+        def saveDB(valor):
             return True
             
         i = some_input()
-        self.assertTrue(guardarDB(cleanSQLI("hohoho" + i)))
+        self.assertTrue(saveDB(cleanSQLI("hohoho" + i)))
                                                     
 if __name__ == '__main__':
     unittest.main()
